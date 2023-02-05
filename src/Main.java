b0005dafc6c1ff2cdbb33f784dac2349ecc6cd5f
laure <lemeyer20@coe.edu>
2023-02-05 14:37:27 -0600

    Done through 5 (I think)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Rational r1;
+        Rational r2;
+        r1 = new Rational(64,10);
+        r2 = new Rational();
+
+        System.out.println(r1);
+        r1.reduce();
+        System.out.println(r1);
 
 
     }
--- src/Rational.java
+++ src/Rational.java
@@ -1,6 +1,77 @@
 public class Rational {
-    private int Numerator;
-    private int Denominator;
+    private int numerator;
+    private int denominator;
 
+    public Rational(){
+        this(0,1);
+    }
 
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int den) {
+        if (den==0){
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        }
+        if (den<0){
+            throw new IllegalArgumentException("Negative is stored in the numerator. Make Denominator Positive.");
+        }
+        this.denominator = den;
+    }
+    public void setNumerator(int num) {
+        this.numerator = num;
+    }
+
+    public String toString()
+    {
+        this.reduce();
+        if (numerator>denominator) {
+            int whole;
+            int remainder;
+            remainder = numerator%denominator;
+            whole = (numerator-remainder)/denominator;
+            return whole + " and " + remainder + "/" + denominator;
+        } else {
+            return numerator + "/" + denominator;
+        }
+    }
+
+    public void reduce(){
+        if (numerator == 0){
+            denominator = 1;
+        } else {
+            int check;
+            boolean found = false;
+            if (numerator < denominator) {
+                check = numerator;
+            } else {
+                check = denominator;
+            }
+            while (check>0 && !found){
+                if((numerator%check==0) && (denominator%check==0)){
+                    numerator = numerator/check;
+                    denominator = denominator/check;
+                    found = true;
+                }
+                check--;
+            }
+        }
+    }
+
+    public Rational(int n, int m){
+        if (m==0){
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        }
+        if (m<0){
+            throw new IllegalArgumentException("Negative is stored in the numerator. Make Denominator Positive.");
+        }
+
+        numerator = n;
+        denominator = m;
+    }
 }
